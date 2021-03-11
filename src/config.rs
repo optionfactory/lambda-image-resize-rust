7b2690c415ecf5a9e5374c922da723cd30e1135c
Francesco Degrassi <francesco.degrassi@optionfactory.net>
2021-03-11 03:51:22 +0100

    :fix: bumped lambda-runtime dep to master, fixed check for originals, support for eu-south-1 region

--- src/config.rs
+++ src/config.rs
@@ -17,7 +17,7 @@ impl Config {
         if let Ok(sizes_string) = env::var("SIZES") {
             for size_string in sizes_string.split(',').into_iter() {
                 let mut parts = size_string.split(":");
-                let folder = parts.next().unwrap().to_string();
+                let folder = parts.next().expect(&sizes_string).to_string();
                 let mut dims = parts.next().unwrap().split('x');
                 let size = (
                     folder,
@@ -28,5 +28,17 @@ impl Config {
         };
 
         sizes
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn it_works() {
+        env::set_var("SIZES", "@1x:200x300,@2x:300x400");
+        let cfg = Config::new();
+        assert_eq!(2, cfg.sizes.len());
+        assert_eq!(200, (cfg.sizes[0]).1);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-#[macro_use]
 extern crate lambda_runtime as lambda;
 #[macro_use]
 extern crate log;
 #[macro_use]
 extern crate serde_derive;
 
+use std::mem;
+use log::LevelFilter;
 use simple_logger::SimpleLogger;
 
 use image::{ImageOutputFormat, ImageError};
@@ -17,24 +18,25 @@ mod config;
 
 use aws_lambda_events::event::s3::{S3Event, S3EventRecord};
 use config::Config;
-use lambda::error::HandlerError;
+use lambda::handler_fn;
 use serde_json::Value;
-use std::error::Error;
 
-fn main() -> Result<(), Box<dyn Error>> {
-    SimpleLogger::new().init().unwrap();
+type Error = Box<dyn std::error::Error + Sync + Send + 'static>;
 
-    lambda!(handle_event);
-
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    SimpleLogger::new().with_level(LevelFilter::Info).init().unwrap();
+    lambda::run(handler_fn(handle_event)).await?;
     Ok(())
 }
 
-fn handle_event(event: Value, _ctx: lambda::Context) -> Result<(), HandlerError> {
+async fn handle_event(event: Value, _ctx: lambda::Context) -> Result<(), Error> {
     let config = Config::new();
-
+    info!("config is: {:?}", config);
     let s3_event: S3Event =
         serde_json::from_value(event)?;
 
+    info!("event is: {:?}", s3_event);
     for record in s3_event.records {
         handle_record(&config, record);
     }
@@ -43,11 +45,15 @@ fn handle_event(event: Value, _ctx: lambda::Context) -> Result<(), HandlerError>
 
 fn handle_record(config: &Config, record: S3EventRecord) {
     let credentials = Credentials::default().expect("Could not retrieve default credentials");
-    let region: Region = record
+    let region_name = record
         .aws_region
-        .expect("Could not get region from record")
+        .expect("Could not get region from record");
+    let region: Region = match region_name.as_str() {
+        "eu-south-1" => Region::Custom { region: "eu-south-1".to_string(), endpoint: "https://s3.eu-south-1.amazonaws.com".to_string() },
+        _ => region_name
         .parse()
-        .expect("Could not parse region from record");
+        .expect("Could not parse region from record"),
+    };
     let bucket = Bucket::new(
         &record
             .s3
@@ -66,7 +72,8 @@ fn handle_record(config: &Config, record: S3EventRecord) {
 
     /* Make sure we don't process files twice */
     let source_path_parts: Vec<&str> = source.split("/").collect();
-    if &"originals" != source_path_parts.last().unwrap() {
+    let last_folder = source_path_parts[source_path_parts.len()-2];
+    if "originals" != last_folder {
         warn!("Source: '{}' not in originals, skipping.", &source);
         return;
     }
@@ -90,14 +97,14 @@ fn handle_record(config: &Config, record: S3EventRecord) {
         .iter()
         .map(|size| {
             let buffer = resize_image(&img, &(size.1, size.2)).expect("Could not resize image");
-            dest_path_parts.pop();
-            dest_path_parts.push(&size.0);
+            let last_folder_pos = dest_path_parts.len() - 2;
+            let _ = mem::replace(&mut dest_path_parts[last_folder_pos], &size.0);
             
             let dest = dest_path_parts.join("/");
             let (_, code) = bucket
                 .put_object_with_content_type_blocking(&dest, &buffer, "image/jpeg")
                 .expect(&format!("Could not upload object to :{}", &dest));
-            info!("Uploaded: {} with: {}", &dest, &code);
+            info!("Uploaded: {} with code: {}", &dest, &code);
         })
         .collect();
 }
@@ -108,4 +115,22 @@ fn resize_image(img: &image::DynamicImage, (new_w, new_h): &(u32, u32)) -> Resul
     scaled.write_to(&mut result, ImageOutputFormat::Png)?;
 
     Ok(result)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn it_works() {
+        let credentials = Credentials::anonymous().unwrap();
+        let region: Region = Region::Custom { region: "eu-south-1".to_string(), endpoint: "https://s3.eu-south-1.amazonaws.com".to_string() };
+        let bucket = Bucket::new(
+            "test-opfa-img-resize",
+            region,
+            credentials,
+        ).expect("Could not get bucket");
+        let (_data, _) = bucket
+        .get_object_blocking("originals/octane.png")
+        .expect(&format!("Could not get object"));
+    }
 }
