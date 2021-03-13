06ae5aeb4e348999cddf008873e171aba2da6c6c
Francesco Degrassi <francesco.degrassi@optionfactory.net>
2021-03-13 19:24:42 +0100

    :new: smartcrop

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ extern crate log;
 #[macro_use]
 extern crate serde_derive;
 
+use std::num::NonZeroU32;
 use std::mem;
 use log::LevelFilter;
 use simple_logger::SimpleLogger;
@@ -20,6 +21,7 @@ use aws_lambda_events::event::s3::{S3Event, S3EventRecord};
 use config::Config;
 use lambda::handler_fn;
 use serde_json::Value;
+use smartcrop::{Analyzer, CropSettings};
 
 type Error = Box<dyn std::error::Error + Sync + Send + 'static>;
 
@@ -43,17 +45,19 @@ async fn handle_event(event: Value, _ctx: lambda::Context) -> Result<(), Error> 
     Ok(())
 }
 
+fn parse_region(region_name: &str) -> Region {
+    match region_name {
+        "eu-south-1" => Region::Custom { region: "eu-south-1".to_string(), endpoint: "https://s3.eu-south-1.amazonaws.com".to_string() },
+        _ => region_name.parse().expect("Could not parse region from record")
+    }
+}
+
 fn handle_record(config: &Config, record: S3EventRecord) {
     let credentials = Credentials::default().expect("Could not retrieve default credentials");
     let region_name = record
         .aws_region
         .expect("Could not get region from record");
-    let region: Region = match region_name.as_str() {
-        "eu-south-1" => Region::Custom { region: "eu-south-1".to_string(), endpoint: "https://s3.eu-south-1.amazonaws.com".to_string() },
-        _ => region_name
-        .parse()
-        .expect("Could not parse region from record"),
-    };
+    let region = parse_region(&region_name);
     let bucket = Bucket::new(
         &record
             .s3
@@ -87,7 +91,7 @@ fn handle_record(config: &Config, record: S3EventRecord) {
         .get_object_blocking(&source)
         .expect(&format!("Could not get object: {}", &source));
 
-    let img = image::load_from_memory(&data)
+    let mut img = image::load_from_memory(&data)
         .ok()
         .expect("Opening image failed");
 
@@ -96,7 +100,7 @@ fn handle_record(config: &Config, record: S3EventRecord) {
         .sizes
         .iter()
         .map(|size| {
-            let buffer = resize_image(&img, &(size.1, size.2)).expect("Could not resize image");
+            let buffer = resize_image(&mut img, &(size.1, size.2)).expect("Could not resize image");
             let last_folder_pos = dest_path_parts.len() - 2;
             let _ = mem::replace(&mut dest_path_parts[last_folder_pos], &size.0);
             
@@ -109,11 +113,21 @@ fn handle_record(config: &Config, record: S3EventRecord) {
         .collect();
 }
 
-fn resize_image(img: &image::DynamicImage, (new_w, new_h): &(u32, u32)) -> Result<Vec<u8>, ImageError> {
+fn resize_image(img: &mut image::DynamicImage, (new_w, new_h): &(u32, u32)) -> Result<Vec<u8>, ImageError> {
+    
+    let an: Analyzer = Analyzer::new(CropSettings::default());
+    let crop_result = an.find_best_crop(
+        img,
+        NonZeroU32::new(*new_w).unwrap(),
+        NonZeroU32::new(*new_h).unwrap(),
+    )
+    .unwrap();
+    let crop = crop_result.crop;
+    
     let mut result: Vec<u8> = Vec::new();
-    let scaled = img.resize(*new_w, *new_h, image::imageops::FilterType::Lanczos3);
+    let cropped = img.crop(crop.x, crop.y, crop.width, crop.height);
+    let scaled = cropped.resize(*new_w, *new_h, image::imageops::FilterType::Lanczos3);
     scaled.write_to(&mut result, ImageOutputFormat::Png)?;
-
     Ok(result)
 }
 
