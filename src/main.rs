19d05836fc5434604e7611ac9078f7a77141b4e4
Francesco Degrassi <francesco.degrassi@optionfactory.net>
2022-02-01 15:00:04 +0100

    :enh: copy original image if format cannot be established

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ use lambda::handler_fn;
 use serde_json::Value;
 
 use lambda_image_resize_rust::{rotation_for, SmarpCropper};
-use image::GenericImageView;
+use image::{GenericImageView, ImageOutputFormat};
 use num_rational::Rational32;
 use num_traits::cast::ToPrimitive;
 
@@ -46,6 +46,8 @@ fn parse_region(region_name: &str) -> Region {
     }
 }
 
+const APPLICATION_OCTET_STREAM: &'static str = "application/octet-stream";
+
 fn handle_record(config: &Config, record: S3EventRecord) {
     let region_name = record
         .aws_region
@@ -71,22 +73,10 @@ fn handle_record(config: &Config, record: S3EventRecord) {
     let (data, _) = source_bucket
         .get_object_blocking(&source)
         .expect(&format!("Could not get object: {}", &source));
-
-    let mut cursor = std::io::Cursor::new(data);
-    let rotate_fn = rotation_for(&mut cursor);
-    cursor.seek(SeekFrom::Start(0)).unwrap();
-    let reader = image::io::Reader::new(cursor)
-        .with_guessed_format()
-        .unwrap();
-    let format = reader.format().unwrap();
-    let img = reader
-        .decode()
-        .expect("Opening image failed");
-    let img = rotate_fn.map(|f| f(&img)).unwrap_or(img);
+    let (metadata, _) = source_bucket.head_object_blocking(&source).expect(&format!("Could not get object metadata: {}", &source));
 
     let source_path_parts: Vec<&str> = source.split("/").collect();
-
-    if  config.dest_bucket == *source_bucket_name {
+    if config.dest_bucket == *source_bucket_name {
         panic!("source and target bucket names are the same. Quitting to avoid an infinite loop.")
     }
     let dest_region = parse_region(&config.dest_region);
@@ -96,6 +86,40 @@ fn handle_record(config: &Config, record: S3EventRecord) {
         Credentials::default().expect("Could not retrieve default credentials"),
     ).expect("Could not dest bucket");
 
+    let dest_path = |folder: &String| {
+        let mut dest_path_parts = source_path_parts.clone();
+        dest_path_parts.insert(source_path_parts.len() - 1, &folder);
+        dest_path_parts.join("/")
+    };
+
+    let mut cursor = std::io::Cursor::new(data);
+    let rotate_fn = rotation_for(&mut cursor);
+    cursor.seek(SeekFrom::Start(0)).unwrap();
+    let reader = image::io::Reader::new(cursor)
+        .with_guessed_format()
+        .unwrap();
+    let format = reader.format();
+    let output_format = format.map(ImageOutputFormat::from).unwrap_or(ImageOutputFormat::Unsupported("No input format".to_owned()));
+
+    if format.is_none() || matches!(output_format, ImageOutputFormat::Unsupported(_)) {
+        info!("Unsupported format, copying source to destinations without processing");
+        let data = reader.into_inner().into_inner();
+        let dest_paths = config.sizes.iter().map(|(folder, _,_)| folder)
+            .chain(config.ratios.iter().map(|(folder, _,_)| folder))
+            .map(dest_path)
+            .collect::<Vec<_>>();
+        for dest_path in dest_paths {
+            let (_, _code) = dest_bucket
+                .put_object_with_content_type_blocking(&dest_path, &data, metadata.content_type.as_ref().map(String::as_str).unwrap_or(APPLICATION_OCTET_STREAM))
+                .expect(&format!("Could not upload object to :{}", &dest_path));
+        }
+        return;
+    }
+    let format = format.unwrap();
+    let img = reader
+        .decode()
+        .expect("Opening image failed");
+    let img = rotate_fn.map(|f| f(&img)).unwrap_or(img);
     let targets = config.sizes.iter()
         .cloned()
         .chain(config.ratios.iter()
@@ -107,9 +131,10 @@ fn handle_record(config: &Config, record: S3EventRecord) {
             }))
         .collect::<Vec<_>>();
 
-    let max_w = targets.iter().map(|(_,w,_)| w).max().cloned().unwrap_or(img.width());
-    let max_h = targets.iter().map(|(_,_,h)| h).max().cloned().unwrap_or(img.height());
-    let mut smart_cropper = SmarpCropper::new(img, Some((max_w,max_h)));
+    let max_w = targets.iter().map(|(_, w, _)| w).max().cloned().unwrap_or(img.width());
+    let max_h = targets.iter().map(|(_, _, h)| h).max().cloned().unwrap_or(img.height());
+
+    let mut smart_cropper = SmarpCropper::new(img, Some((max_w, max_h)));
     for (folder, w, h) in targets {
         let cropped_image = smart_cropper.crop(w, h);
         let mut buffer = Vec::new();
@@ -123,7 +148,7 @@ fn handle_record(config: &Config, record: S3EventRecord) {
             image::ImageFormat::Png => "image/png",
             image::ImageFormat::Jpeg => "image/jpeg",
             image::ImageFormat::Gif => "image/gif",
-            _ => "application/octet-stream"
+            _ => APPLICATION_OCTET_STREAM,
         };
 
         let (_, code) = dest_bucket
@@ -131,7 +156,6 @@ fn handle_record(config: &Config, record: S3EventRecord) {
             .expect(&format!("Could not upload object to :{}", &dest));
         info!("Uploaded: {} with code: {}", &dest, &code);
     }
-
 }
 
 
